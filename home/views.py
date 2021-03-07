663381fb12e7d1af28b031fe8207901a71388c25
“Philip <2479229z@student.gla.ac.uk>
2021-03-07 00:00:04 +0000

    citypage view setup + attraction card created

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from home.models import City, Attraction
+
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html', context={})
+
+def citypage(request, NameSlug):
+    ctx = {}
+    ctx['city'] = City.objects.get(NameSlug=NameSlug)
+    ctx['attractions'] = Attraction.objects.order_by('-Views')
+    return render(request, 'citypage.html', context=ctx)
