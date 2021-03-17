6ac63266678e9efb5930c58372dd468f8b2ae5c6
PhilipZubel <49106470+PhilipZubel@users.noreply.github.com>
2021-03-17 20:20:32 +0000

    view setpu

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contact/', views.contactus, name="contact"),
     path('cities/<slug:NameSlug>/<str:sortBy>/', views.citypage, name="citypage"),
     path('random/', views.send_somewhere_random, name="random"),
+    path('myattractions/', views.myattractions, name="myattractions"),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
 
 from django.shortcuts import render, redirect
-from home.models import City, Attraction
+from home.models import City, Attraction, AttractionReviews, MVUser, User
 from django.http import Http404
 from random import randint
+from django.contrib.auth.decorators import login_required
 
-from home.models import City, Attraction, AttractionReviews
 
 # Create your views here.
 
@@ -17,7 +17,6 @@ def homepage(request):
     ctx['cities'] = City.objects.order_by('-Views')[:10]
     ctx['attractions'] = Attraction.objects.order_by('-Views')[:10]
     return render(request, 'homepage.html', context=ctx)
-
 
 def send_somewhere_random(request):
     rand_attractions = Attraction.objects.all()
@@ -48,32 +47,22 @@ def citypage(request, NameSlug, sortBy):
     # elif sortBy.lower() == "rating":
     #     ctx['attractions'] = attractions.order_by('-Views') # sort by average rating
     #     ctx['dropdown_msg'] = 'Top Rated'
-    # elif sortBy.lower() == "date":
-    #     ctx['attractions'] = attractions.order_by('-Views') # sort by date
-    #     ctx['dropdown_msg'] = 'Newest First'
+    elif sortBy.lower() == "date":
+        ctx['attractions'] = attractions.order_by('-Added') # sort by date
+        ctx['dropdown_msg'] = 'Newest First'
     else:
         ctx['attractions'] = attractions.all()
         ctx['dropdown_msg'] = 'Sorted By:'
     
 
-
-    # don't look at comments below
-
-    # if(request.user):
-    #     my_rating =  AttractionReviews.objects.filter(UserReviewing=request.user).first()
-
-    # ratings = AttractionRatings.objects.filter(CityRated=city)
-
-    # ratings_sum = 0
-    # ratings_ave = 0
-    # ratings_count = ratings.count()
-    # if(ratings_count>0):
-    #     for rating in ratings:
-    #         ratings_sum += rating.Rating
-    #     ratings_ave = rating.Rating/ratings.count()
-    # ctx['avg_rating'] = ratings_ave + 0.5
-
-
     return render(request, 'citypage.html', context=ctx)
 
+#@login_required(login_url="/login")
+def myattractions(request):
+    print(request.user)
+    user = MVUser.objects.get(DjangoUser=request.user)
+    attractions = user.SavedAttractions.order_by("City")
+    ctx={}
+    ctx["attractions"] = attractions.all()
+    return render(request, 'myattractions.html', context=ctx)
 
