3e32cf8f431747bf4179a4e3c37c8b2f2b2da81c
Aaron Milne <2466574M@student.gla.ac.uk>
2021-03-18 15:24:43 +0000

    Fixed the issue where only the first city in the list could have stars show

--- static/scripts/rateAttraction.js
+++ static/scripts/rateAttraction.js
@@ -1,61 +1,45 @@
 //get all stars
-var one = document.getElementById("1")
-var two = document.getElementById("2")
-var three = document.getElementById("3")
-var four = document.getElementById("4")
-var five = document.getElementById("5")
+var one = document.getElementsByName("1")
+var two = document.getElementsByName("2")
+var three = document.getElementsByName("3")
+var four = document.getElementsByName("4")
+var five = document.getElementsByName("5")
 
-var form = document.querySelector('.rate_form')
+var form = document.getElementsByClassName('rate_form')
 var csrf = document.getElementsByName('csrfmiddlewaretoken')
 
 
-function handleStarSelect(size) {
-	const children = form.children
-	for (let i=0; i<children.length; i++) {
-		if (i <= size) {
-			children[i].classList.add('checked')
-		} else {
-			children[i].classList.remove('checked')
+function handleStarSelect(size, city) {
+	for (const cityForm of form) {
+		if (cityForm.id === city) {
+			const children = cityForm.children;
+
+			for (let i = 0; i < children.length; i++) {
+				if (size !== 0) {
+					if (i <= size) {
+						children[i].classList.add('checked');
+					} else {
+						children[i].classList.remove('checked');
+					}
+				} else {
+					children[i].classList.remove('checked');
+				}
+			}
 		}
 	}
 }
 
-const handleSelect = (selection) => {
-	switch(selection) {
-		case "1": {
-			handleStarSelect(1)
-			return
-		}
-		case "2": {
-			handleStarSelect(2)
-			return
-		}
-		case "3": {
-			handleStarSelect(3)
-			return
-		}
-		case "4": {
-			handleStarSelect(4)
-			return
-		}
-		case "5": {
-			handleStarSelect(5)
-			return
-		}
-	}
-}
-
-function getNumericValue(stringValue) {
-	var numericValue = parseInt(stringValue)
-	return numericValue
-}
 
 if (one) {
-	const a = [one, two, three, four, five]
+	const a = [one, two, three, four, five];
 
-	a.forEach(item => item.addEventListener('mouseover', (event) => {
-		handleSelect(event.target.id)
-	}))
+	for (let j=1; j<=5; j++) {
+		for (const star of a) {
+			star[j-1].addEventListener('mouseover', function(){handleStarSelect(event.target.name, event.target.value)});
+
+			star[j-1].addEventListener('mouseleave', function(){handleStarSelect(0, event.target.value)})
+		}
+	}
 }
 
 
@@ -65,6 +49,7 @@ function showNotLoggedInAlert() {
 
 
 function rateAttraction() {
+	const a = [one, two, three, four, five]
 
 	a.forEach(item=> item.addEventListener('click', (event)=>{
 		var val = event.target.id
