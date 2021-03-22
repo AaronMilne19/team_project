4439af735979a7cad0c77c6edf9c9e690baf7d59
Ben Zour <2474132z@student.gla.ac.uk>
2021-03-22 16:13:03 +0100

    hotfixing displaying pictures, couldn't parse their urls before + adding some city pics

--- team_project/settings.py
+++ team_project/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Account.context_peocessors.userinfo'
+                'Account.context_peocessors.userinfo',
+                'django.template.context_processors.media',
             ],
         },
     },
