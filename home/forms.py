1b450bfb1661a169ccc729478eebd1b77ca40b5b
Aaron Milne <2466574M@student.gla.ac.uk>
2021-03-29 14:24:51 +0100

    added help text to rating

--- home/forms.py
+++ home/forms.py
@@ -6,7 +6,7 @@ from durationwidget.widgets import TimeDurationWidget
 
 class ReviewForm(forms.ModelForm):
     Title = forms.CharField(max_length=100)
-    Rating = forms.IntegerField(max_value=5, min_value=1)
+    Rating = forms.IntegerField(max_value=5, min_value=1, help_text="Rating between 1-5")
     DateVisited = forms.DateField(widget=NumberInput(attrs={'type': 'date'}), label="Date visited")
     TimeTaken = forms.DurationField(widget=TimeDurationWidget(show_days=False, show_seconds=False), initial='0:0', label="Time taken to explore")
     Comment = forms.CharField(max_length=1000, widget=forms.Textarea(attrs={'rows':15}), label="More details")
